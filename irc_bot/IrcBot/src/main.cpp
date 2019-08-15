0be04e3f2e623ab61039c695e4510f24df841025
Piotr Krupa <piotrkrupa06@gmail.com>
2019-08-15 15:43:10 +0200

    Disabled uptime in log

--- irc_bot/IrcBot/src/main.cpp
+++ irc_bot/IrcBot/src/main.cpp
@@ -5,6 +5,7 @@
 int main()
 {
 	loguru::g_preamble_thread = false;
+	loguru::g_preamble_uptime = false;
 	loguru::add_file("chat_and_logs.log", loguru::Append, loguru::Verbosity_MAX);
 	loguru::add_file("logs.log", loguru::Append, loguru::Verbosity_8);
 
